7af3c12a1be65850cb865bd3b4d2ba5259ab54f8
Gabriel Redig <glredig@gmail.com>
2013-01-19 21:48:20 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OdtApp::Application.config.secret_token = '26c0afa500d95c57d14e1e5b169527f7458c2691e6cd6c88801aa828884aea9c581dcf0a503c3dc49f8dc6921676f37aee65bec768ca3f8125f475ecb602052e'
