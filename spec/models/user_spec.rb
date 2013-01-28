09f496427b187cd1bb74327674ac7864d098a5c6
Gabriel Redig <glredig@gmail.com>
2013-01-27 20:50:39 -0800

    Create basic user model with passwords

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,17 @@
 require 'spec_helper'
 
 describe User do
-  before { @user = User.new(name: "John Smith", email: "jsmith@example.com") }
+  before { @user = User.new(name: "John Smith", email: "jsmith@example.com", 
+                            password: "foobar", password_confirmation: "foobar") }
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -58,5 +63,45 @@ describe User do
   	end
 
   	it { should_not be_valid }
+  end
+
+  describe "when password field is blank" do
+    before { @user.password = @user.password_confirmation = " " }
+
+    it { should_not be_valid }
+  end
+
+  describe "when password and confirmation don't match" do
+    before { @user.password_confirmation = "notthesame" }
+
+    it { should_not be_valid }
+  end
+
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
+
+    it { should_not be_valid }
+  end
+
+  describe "with a password that's too short" do
+    before { @user.password_confirmation = @user.password = "a" * 5 }
+
+    it { should be_invalid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by_email(@user.email) }
+
+    describe "with valid password" do
+      it { should == found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not == user_for_invalid_password }
+      specify { user_for_invalid_password.should be_false }
+    end
   end
 end
