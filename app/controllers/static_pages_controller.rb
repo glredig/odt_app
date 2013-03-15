1afa29e1f9a05f9d61b4ea53a850c39af333be32
Gabriel Redig <glredig@gmail.com>
2013-03-15 00:03:41 -0700

    Add images to about page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,11 @@
 class StaticPagesController < ApplicationController
 	def about
-		@curtis_pic_1 = "https://s3.amazonaws.com/onlinedrumteacher/curtis_about_1.jpg"
+		@curtis_pic_1 = "https://onlinedrumteacher.s3.amazonaws.com/images/curtis_about_1.jpg"
+		@curtis_pic_2 = "https://onlinedrumteacher.s3.amazonaws.com/images/curtis_about_2.jpg"
+		@curtis_pic_3 = "https://onlinedrumteacher.s3.amazonaws.com/images/curtis_about_3.jpg"
+		@curtis_pic_4 = "https://onlinedrumteacher.s3.amazonaws.com/images/curtis_about_4.jpg"
+		@curtis_pic_5 = "https://onlinedrumteacher.s3.amazonaws.com/images/curtis_about_5.jpg"
+		@curtis_pic_2 = "https://onlinedrumteacher.s3.amazonaws.com/images/curtis_about_6.jpg"
 	end
 
 	def lessons
