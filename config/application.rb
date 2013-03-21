e3f74fd51b5ce45c035013371dd09532c9f6f11c
Gabriel Redig <glredig@gmail.com>
2013-03-20 22:49:25 -0700

    Put carrier wave config in development

--- config/application.rb
+++ config/application.rb
@@ -65,13 +65,6 @@ module OdtApp
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.before_configuration do 
-        env_file = File.join(Rails.root, 'config', 'local_env.yml')
-        YAML.load(File.open(env_file)).each do |key, value|
-            ENV[key.to_s] = value
-        end if File.exists?(env_file)
-    end
-
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
     config.assets.initialize_on_precompile = false
