7e75a2a66f0b37608f12469b63b6d39a7dca784a
Gabriel Redig <glredig@gmail.com>
2013-05-02 13:13:26 -0700

    add spec for written lessons

--- spec/models/written_lesson_spec.rb
+++ spec/models/written_lesson_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe WrittenLesson do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # let(:user) { FactoryGirl.create(:user) }
+  before { @written_lesson = WrittenLesson.create(title: "Written 1", content: "This is Written Lesson 1") }
+
+  subject { @written_lesson }
+
+  it { should respond_to(:title) }
+  it { should respond_to(:content) }
+
+  describe "when it doesn't have a title" do
+  	before { @written_lesson.title = nil }
+  	it { should_not be_valid }
+  end
+
+  describe "when it doesn't have content" do
+  	before { @written_lesson.content = nil }
+  	it { should_not be_valid }
+  end
 end
+
