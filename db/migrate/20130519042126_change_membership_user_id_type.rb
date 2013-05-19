d27ddc52d38201d45a5c662a4fd45c78effef28b
Gabriel Redig <glredig@gmail.com>
2013-05-18 23:18:59 -0700

    migration cleanup

--- db/migrate/20130519042126_change_membership_user_id_type.rb
+++
@@ -1,9 +0,0 @@
-class RemoveMembershipUserId < ActiveRecord::Migration
-  def up
-  	remove_column :memberships, :user_id
-  end
-
-  def down
-  	add_column :memberships, :user_id, :string
-  end
-end
